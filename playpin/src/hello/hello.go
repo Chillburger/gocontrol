9ce84a3edd3a0047879158e1eae10e44907929da
Chillburger <letitbespliced7@gmail.com>
2018-01-07 22:22:18 -0500

    continuing gotutorial

--- playpin/src/hello/hello.go
+++ playpin/src/hello/hello.go
@@ -7,8 +7,20 @@ import (
   "math"
 )
 
-func add (x int, y int) int {
+var i, j int = 1, 2
+
+func add (x , y int) int {
   return x + y
+}
+
+func swap( x, y string) (string, string) {
+  return y, x
+}
+
+func split ( sum int) (x, y int) {
+  x = sum * 4 / 9
+  y = sum - x
+  return
 }
 
 func main() {
@@ -18,5 +30,14 @@ func main() {
   fmt.Println("My Favorite number is: ", rand.Intn(10))
   fmt.Println("test math:", math.Sqrt(7))
   fmt.Println(add(42, 13))
+
+  fmt.Println("pi:" , math.Pi)
+  a, b := swap("hello", "world")
+  fmt.Println("swap funct: ", a, b)
+
+  fmt.Println(split(17))
+  var c, python, java= true, false, "no!"
+  k := 3
+  fmt.Println(i, k, j, c, python, java)
 
 }
