8bff6b071aa9d1fa7c5899ae1984c047b042cdc5
Chillburger <letitbespliced7@gmail.com>
2018-01-10 18:22:46 -0500

    finished the flow control lesson

--- playpin/src/flowcontrol/flowcontrol.go
+++ playpin/src/flowcontrol/flowcontrol.go
@@ -4,6 +4,7 @@ import (
   "fmt"
   "math"
   "runtime"
+  "time"
 )
 
 // demonstrates an if statement, braces are required, the expression does not need pararens
@@ -105,7 +106,46 @@ func main() {
   case "linux":
     fmt.Println("Linux")
   default:
-    fmt.Printf("%s", os)
+    fmt.Printf("%s\n", os)
   }
 
+
+  // switch evaluation order
+  fmt.Println("When's Saturday?")
+  today := time.Now().Weekday()
+  switch time.Saturday {
+  case today + 0:
+    fmt.Println("Today")
+  case today + 1:
+    fmt.Println("Tomorrow")
+  case today + 2:
+    fmt.Println("Two Days")
+  default:
+    fmt.Println("Too far away.")
+  }
+
+  // swtich statement with no conditional
+  t := time.Now()
+  switch {
+  case t.Hour() < 12:
+    fmt.Println("Good morning")
+  case t.Hour() < 17:
+    fmt.Println("Good afternoon")
+  default:
+    fmt.Println("Good evening")
+  }
+
+  // switch statement defer
+
+  defer fmt.Println("world")
+
+  fmt.Println("hello")
+
+  // stacking defers
+  fmt.Println("counting")
+
+  for counter := 0; counter < 10; counter++ {
+    defer fmt.Println(counter)
+  }
+  fmt.Println("done")
 }
