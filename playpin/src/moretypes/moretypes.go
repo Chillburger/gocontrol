c3342cb46ae8f9dc595e4efaab927b96c60ffc2a
Chillburger <letitbespliced7@gmail.com>
2018-01-11 21:14:49 -0500

    adding slice and array lessions

--- playpin/src/moretypes/moretypes.go
+++ playpin/src/moretypes/moretypes.go
@@ -47,4 +47,81 @@ func main() {
 
     // from struct literals
     fmt.Println(v1, point, v2, v3)
+
+
+    // demonstration of arrays
+    // this is an array of two strings
+    var a [2]string
+    a[0] = "hello"
+    a[1] = "teamo"
+    fmt.Println(a[0], a[1])
+    fmt.Println(a)
+
+    primes := [6]int{ 2, 3, 5, 7, 11, 13}
+    fmt.Println(primes)
+
+    // Slices is a dynamically sized flexible view into elements of an arrays
+    // they are much more common in implementation
+    var s []int = primes[1:4]
+
+    fmt.Println(s)
+
+    // a slice does not store data, it just describes a section of an underlying arrays
+    // changing elements of a silce modifies the corresponding elements of the underlying arrays
+    names := [4]string {
+      "John",
+      "Paul",
+      "Ringo",
+      "George",
+    }
+
+    fmt.Println(names)
+
+    aa := names[0:2]
+    bb := names[1:3]
+
+    fmt.Println(aa, bb)
+
+    bb[0] = "XXX"
+    fmt.Println(aa, bb)
+    fmt.Println(names)
+
+    // slice literals, like an array literal without the length
+
+    q := []int{2, 3, 5, 7, 11, 13}
+
+    fmt.Println(q)
+
+    r := []bool{true, false, true, true, false, true}
+
+    fmt.Println(r)
+
+    ss := []struct {
+      i int
+      b bool
+    }{
+      {2, true},
+      {3, false},
+      {5, true},
+      {7, true},
+      {11, false},
+      {13, true},
+    }
+
+    fmt.Println(ss)
+
+    // slice defaults, you can omit the high or low bounds to use their defaults instead
+
+    slicer := []int{2, 3, 5, 7, 11, 13}
+    slicer = slicer[1:4]
+
+    fmt.Println(slicer)
+
+    slicer = slicer[:2]
+    fmt.Println(slicer)
+
+
+    slicer = slicer[1:]
+    fmt.Println(slicer)
+
 }
