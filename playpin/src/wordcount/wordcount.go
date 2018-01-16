b3a632b7f9642857c6913042fb0888bff4a606bd
Chillburger <letitbespliced7@gmail.com>
2018-01-15 19:21:14 -0500

    finished word count exercise

--- playpin/src/wordcount/wordcount.go
+++ playpin/src/wordcount/wordcount.go
@@ -3,13 +3,29 @@
 package main
 
 import (
+  "fmt"
   "golang.org/x/tour/wc"
+  "strings"
 )
 
 func WordCount (s string) map[string]int {
-  return map[string]int{"x": 1}
+  wordArray := strings.Fields(s)
+  wordCount := make(map[string]int)
+  for i := 0; i < len(wordArray); i ++ {
+    word, check := wordCount[wordArray[i]]
+    if check == true {
+      fmt.Println("word found, adding to counter:", wordArray[i], word)
+      wordCount[wordArray[i]] = wordCount[wordArray[i]] + 1
+    } else {
+      fmt.Println("new word found:", wordArray[i], word)
+      wordCount[wordArray[i]] = 1
+    }
+  }
+
+  fmt.Println(wordCount)
+  return wordCount
 }
 
-func main {
+func main() {
   wc.Test(WordCount)
 }
